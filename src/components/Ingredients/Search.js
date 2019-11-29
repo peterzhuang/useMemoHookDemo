71e924815c7de98322420d92e74e7cf6a4776978
yuchuan zhuang <yuchuan.zhuang@mindgeek.com>
2019-11-29 17:45:01 -0500

    Finish L480 Using custom hooks and include client-side remove ingredient by inserting uuid instead of server retrieved name field

--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,12 +1,16 @@
 import React, { useState, useEffect, useRef } from 'react';
 
 import Card from '../UI/Card';
+import ErrorModal from '../UI/ErrorModal';
 import './Search.css';
+import useHttp from '../../hooks/http';
 
 const Search = React.memo(props => {
   const { onLoadIngredients } = props;
   const [enteredFilter, setEnteredFilter] = useState('');
   const inputRef = useRef();
+
+  const {isLoading, data, error, sendRequest, clear } = useHttp();
 
   useEffect(() => {
     console.log("re-run due to onLoadIngredients change!");
@@ -16,33 +20,67 @@ const Search = React.memo(props => {
           enteredFilter.length === 0
             ? ''
             : `?orderBy="title"&equalTo="${enteredFilter}"`;
-        fetch(
-          'https://react-hooks-update.firebaseio.com/ingredients.json' + query
-        )
-          .then(response => response.json())
-          .then(responseData => {
-            const loadedIngredients = [];
-            for (const key in responseData) {
-              loadedIngredients.push({
-                id: key,
-                title: responseData[key].title,
-                amount: responseData[key].amount
-              });
-            }
-            onLoadIngredients(loadedIngredients);
-          });
+
+        sendRequest('https://react-hooks-update.firebaseio.com/ingredients.json'+ query, 'GET');
+
+
+
+        // const loadedIngredients = [];
+        //     for (const key in responseData) {
+        //       loadedIngredients.push({
+        //         id: key,
+        //         title: responseData[key].title,
+        //         amount: responseData[key].amount
+        //       });
+        //     }
+        //     onLoadIngredients(loadedIngredients);
+          
+            
+        // fetch(
+        //   'https://react-hooks-update.firebaseio.com/ingredients.json' + query
+        // )
+        //   .then(response => response.json())
+        //   .then(responseData => {
+        //     const loadedIngredients = [];
+        //     for (const key in responseData) {
+        //       loadedIngredients.push({
+        //         id: key,
+        //         title: responseData[key].title,
+        //         amount: responseData[key].amount
+        //       });
+        //     }
+        //     onLoadIngredients(loadedIngredients);
+        //   });
       }
     }, 500);
     return () => {
       clearTimeout(timer);
     };
-  }, [enteredFilter, onLoadIngredients, inputRef]);
+  }, [enteredFilter, inputRef, sendRequest]);
+
+  useEffect(() => {
+    if(!isLoading && !error && data) {
+          const loadedIngredients = [];
+          for (const key in data) {
+            loadedIngredients.push({
+              id: key,
+              title: data[key].title,
+              amount: data[key].amount
+            });
+          }
+          onLoadIngredients(loadedIngredients);
+    }
+  }, [data, isLoading, error, onLoadIngredients]);
 
   return (
     <section className="search">
+      {error && (
+        <ErrorModal onClose={clear}>{error}</ErrorModal>
+      )}
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
+          {isLoading && <span>Loading...</span>}
           <input
             ref={inputRef}
             type="text"
---
+++ src/hooks/http.js
@@ -0,0 +1,76 @@
+import { useReducer, useCallback } from 'react';
+
+const initialState = {
+    loading: false,
+    error: null,
+    data: null,
+    extra: null,
+    identifier: null,
+  };
+
+const httpReducer = (curHttpState, action) => {
+    switch (action.type) {
+      case 'SEND':
+        return { loading: true, error: null, data: null, extra: null, identifier: action.identifier };
+      case 'RESPONSE':
+        return { ...curHttpState, loading: false, data: action.responseData, extra: action.extra };
+      case 'ERROR':
+        return { loading: false, error: action.errorMessage };
+    //   case 'CLEAR':
+    //     return { ...curHttpState, error: null };
+      case 'CLEAR':
+          return initialState;
+      default:
+        throw new Error('Should not be reached!');
+    }
+  };
+
+const useHttp = () => {
+  const [httpState, dispatchHttp] = useReducer(httpReducer, initialState);
+
+  const clear = useCallback(() => dispatchHttp({ type: 'CLEAR'}), []);
+
+  const sendRequest = useCallback((url, method, body, reqExtra, reqIdentifier) => {
+    dispatchHttp({ type: 'SEND', identifier: reqIdentifier });
+    fetch(
+        url,
+        {
+          method: method,
+          body: body,
+          headers: {
+              'Content-Type': 'application/json'
+          }
+        }
+      )
+        .then(response => {
+            return response.json();
+         // dispatchHttp({ type: 'RESPONSE' });
+          // setUserIngredients(prevIngredients =>
+          //   prevIngredients.filter(ingredient => ingredient.id !== ingredientId)
+          // );
+        //   dispatch({ type: 'DELETE', id: ingredientId });
+
+        })
+        .then(responseData => {
+            console.log("dispatching RESPONSE!");
+            dispatchHttp({ type: 'RESPONSE', responseData: responseData, extra: reqExtra });
+        })
+        .catch(error => {
+            console.log("dispatching ERROR!");
+          dispatchHttp({ type: 'ERROR', errorMessage: 'Something went wrong!' });
+        });
+  }, []);
+    return {
+        isLoading: httpState.loading,
+        data: httpState.data,
+        error: httpState.error,
+        sendRequest: sendRequest,
+        reqExtra: httpState.extra,
+        reqIdentifier: httpState.identifier,
+        clear: clear
+    };
+};
+
+
+
+export default useHttp;
